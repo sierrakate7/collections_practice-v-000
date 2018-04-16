df945a908776caf9c2744d490ea5df32899238f2
Sierra McCahon <smitche4@mail.umw.edu>
2018-04-16 02:06:21 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
+def sort_array_asc(array)
+  array.sort
+end 
 
+def sort_array_desc(array)
+  array.sort.reverse
+end 
+
+def sort_array_char_count
+  arrray.sort do 
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array)
+  array.each do |letter|
+    letter[2] = puts "$"
+  end
+end
