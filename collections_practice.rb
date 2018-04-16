94867a39e91d4cc3af5cef3fd93f1e6f6394b6a7
Sierra McCahon <smitche4@mail.umw.edu>
2018-04-16 02:13:29 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,9 +9,11 @@ def sort_array_desc(array)
 end
 
 
-def sort_array_char_count
-  arrray.sort do 
-end 
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
